3c3f773135dd84fcd014c5a9213862dd91738e4d
t49s94 <86166259+t49s94@users.noreply.github.com>
2025-02-22 07:37:48 +0000

    added

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route('/')
 def hello_world():
   return 'Hello, World!'
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
